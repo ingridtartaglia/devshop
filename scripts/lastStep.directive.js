fcd5380363cc90200f05af2017825303cec19e62
Ingrid Tartaglia <ingrid.tartaglia@gmail.com>
2015-12-20 11:27:42 -0200

    criando diretiva da página final

--- scripts/lastStep.directive.js
+++ scripts/lastStep.directive.js
@@ -3,5 +3,59 @@ angular
     .directive('lastStep', lastStep);
 
 function lastStep() {
-    /* implementation details */
-}
+    var directive = {
+        templateUrl: '/views/lastStep.html',
+        restrict: 'E',
+        controller: lastStepCtrl,
+        controllerAs: 'lastStep',
+        bindToController: {
+            sum: '='
+        }
+    };
+    return directive;
+};
+
+/////
+
+lastStepCtrl.$inject = ['$scope', 'Developers', 'Organization'];
+
+function lastStepCtrl($scope, Developers, Organization) {
+    var vm = this;
+
+    vm.developersCart = Developers;
+    vm.errorOnValidate = false;
+    vm.orderSuccess = false;
+
+    vm.shopDevelopers = shopDevelopers;
+    vm.sumSubtotal = sumSubtotal;
+    vm.validate = validate;
+
+    vm.developersCart.$watch(sumSubtotal);
+
+    ////
+
+    function shopDevelopers(){
+        vm.orderSuccess = true;
+    };
+
+    function sumSubtotal(){
+        vm.sum = 0;
+        vm.developersCart.forEach(function(developer){
+            if (developer.hour != null) {
+                vm.sum = vm.sum + (developer.hour * developer.price);
+            }
+        });
+    };
+
+    function validate(){
+        if (vm.discountCode === "SHIPTO") {
+            vm.sum = vm.sum * 0.7;
+            vm.errorOnValidate = false;
+            vm.hideDiscountForm = true;
+        }
+        else {
+            vm.errorOnValidate = true;
+        }
+    }
+
+};
