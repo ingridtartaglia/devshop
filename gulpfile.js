afb74dd3c5a2088a026ebcc98b55648b6806496d
Ingrid Tartaglia <ingrid.tartaglia@gmail.com>
2015-12-21 12:58:36 -0200

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,28 @@ var wiredep = require('wiredep').stream;
 var inject = require('gulp-inject');
 var server = require('gulp-server-livereload');
 var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var lib    = require('bower-files')();
+var minifyCss = require('gulp-minify-css');
+var useref = require('gulp-useref');
+var gif = require('gulp-if');
+/////////////////// BUILD SCRIPTS
 
+gulp.task('build', function(){
+    gulp.src('./bower_components/font-awesome/fonts/*')
+        .pipe(gulp.dest('dist/fonts'));
 
+    gulp.src('./views/*.html')
+        .pipe(gulp.dest('dist/views'));
+
+  return gulp.src('./index.html')
+            .pipe(useref())
+            .pipe(gif('*.js', uglify()))
+            .pipe(gulp.dest('dist'));
+});
+
+/////////////////// DEV SCRIPTS
 gulp.task('init', ['webserver', 'inject', 'sass:watch']);
 
 gulp.task('webserver',function() {
